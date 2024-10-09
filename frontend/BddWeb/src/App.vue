941e7d4fc022f0403411c01f3d83d187465475f2
KristijanRoss <kristijan.ros@gmail.com>
2024-10-09 21:29:30 +0200

    [BddWeb] - frontend init

---
+++ frontend/BddWeb/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const message = ref("");
+
+fetch("https://localhost:7283/api/auth/get")
+  .then((response) => response.text())
+  .then((data) => {
+    console.log(data);
+    message.value = data;
+  });
+</script>
+
+<template>
+  {{ message }}
+</template>
