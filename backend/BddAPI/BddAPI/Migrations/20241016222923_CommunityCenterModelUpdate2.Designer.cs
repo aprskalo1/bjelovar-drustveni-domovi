d0c4c8d4c9c53ed551fa5d58af5ad6cdc2e77c18
Ante Prskalo <aprskalo@racunarstvo.hr>
2024-10-17 00:29:55 +0200

    [BddAPI] - migrations added

--- backend/BddAPI/BddAPI/Migrations/20241016222923_CommunityCenterModelUpdate2.Designer.cs
+++ backend/BddAPI/BddAPI/Migrations/20241016222923_CommunityCenterModelUpdate2.Designer.cs
@@ -4,6 +4,7 @@ using BddAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BddAPI.Migrations
 {
     [DbContext(typeof(BddDbContext))]
-    partial class BddDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241016222923_CommunityCenterModelUpdate2")]
+    partial class CommunityCenterModelUpdate2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/BddAPI/BddAPI/Migrations/20241016222923_CommunityCenterModelUpdate2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BddAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommunityCenterModelUpdate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "CommunityCenters",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Price",
+                table: "CommunityCenters",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
